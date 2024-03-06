afbdca8e063ebb0ae278a4eca73b05cba4b0db9d
Samuel <samuelfs96@gmail.com>
2024-03-06 02:13:27 -0400

    feat(frontend): :camera_flash: config dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">my task app</h1>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center dark:bg-neutral-900">
+      <button className="bg-gray-200 hover:bg-gray-300 rounded-md text-sm py-2 px-6 hover:border-gray-400 border-2 dark:text-white dark:bg-neutral-600">
+        dark mode
+      </button>
+    </div>
+  );
 }
 
 export default App;
