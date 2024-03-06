e5e77afb30f8318b33a65e74398df28c680124ea
Samuel <samuelfs96@gmail.com>
2024-03-05 23:07:26 -0400

    feat(backend): first commit

---
+++ tasks_app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.utcnow)
+
+    def __str__(self):
+        return str(self.title)
