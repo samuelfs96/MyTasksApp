27df7fdbce954f834001fbbce8c1c9843ccff153
Samuel <samuelfs96@gmail.com>
2024-03-06 11:27:22 -0400

    feat(backend): :card_file_box: change id to uuid field on task model

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -1,8 +1,13 @@
+import uuid
 from datetime import datetime
 from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+    id = models.UUIDField(
+         primary_key = True,
+         default = uuid.uuid4,
+         editable = False)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
