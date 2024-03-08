bc3c57fb793bf5a4ce194d2ca3d6b92c2e1f299c
Samuel <samuelfs96@gmail.com>
2024-03-08 07:58:05 -0400

    feat(backend): :necktie: change destroy method on taskview for custom response messages

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+import traceback
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.http import Http404
 from .serializer import TaskSerializer
 from .models import Task
 
@@ -6,3 +9,13 @@ from .models import Task
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response({'message': '¡Tarea eliminada exitosamente!'}, status=status.HTTP_200_OK)
+        except Http404:
+            return Response({'message': '¡Tarea no encontrada!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            traceback.print_exc()
+            raise ex
