43ea94720f009d4e16dd772357a706dab97829dc
Samuel <samuelfs96@gmail.com>
2024-03-08 23:13:22 -0400

    feat(backend): :necktie: response queryset on update method, and update custom messages for error cases

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -16,6 +16,19 @@ class TaskView(viewsets.ModelViewSet):
             return Response({'message': '¡Tarea eliminada exitosamente!'}, status=status.HTTP_200_OK)
         except Http404:
             return Response({'message': '¡Tarea no encontrada!'}, status=status.HTTP_404_NOT_FOUND)
-        except Exception as ex:
+        except Exception:
             traceback.print_exc()
-            raise ex
+            return Response({'message': '¡Error al eliminar la tarea!'}, status=status.HTTP_400_BAD_REQUEST)
+    def update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            updated_records = self.get_queryset()
+            return Response(self.get_serializer(updated_records, many=True).data, status=status.HTTP_200_OK)
+        except Http404:
+            return Response({'message': '¡Tarea no encontrada!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            traceback.print_exc()
+            return Response({'message': '¡Error al actualizar la tarea!'}, status=status.HTTP_400_BAD_REQUEST)
